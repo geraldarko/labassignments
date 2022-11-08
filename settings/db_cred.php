d6d9f21de822b840d4f9ec58838d324643e5dee7
geraldarko <gerald.darko@ashesi.edu.gh>
2022-11-08 16:09:19 +0000

    Update db_cred.php

--- settings/db_cred.php
+++ settings/db_cred.php
@@ -23,6 +23,10 @@ define("SERVER", "localhost");
 define("USERNAME", "root");
 define("PASSWD", "");
 
+define("DATABASE-1", "heroku_6ad55d9ffb3eba4");
+define("SERVER-1", "us-cdbr-east-06.cleardb.net");
+define("USERNAME-1", "b9c65028092515");
+define("PASSWD-1", "9bbf3880");
 
 ?>
 
