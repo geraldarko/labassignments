aecd981e36357705db9f937cd41e82034f0c2644
geraldarko <gerald.darko@ashesi.edu.gh>
2022-11-08 16:10:49 +0000

    Update db_cred.php

--- settings/db_cred.php
+++ settings/db_cred.php
@@ -11,6 +11,11 @@ define("SERVER", "us-cdbr-east-06.cleardb.net");
 define("USERNAME", "b9c65028092515");
 define("PASSWD", "9bbf3880");
 
+define("DATABASE", "shoppn");
+define("SERVER", "localhost");
+define("USERNAME", "root");
+define("PASSWD", "");
+
   -->
 
 
@@ -18,15 +23,12 @@ define("PASSWD", "9bbf3880");
 <?php
 //Database credentials
 
-define("DATABASE", "shoppn");
-define("SERVER", "localhost");
-define("USERNAME", "root");
-define("PASSWD", "");
 
-define("DATABASE-1", "heroku_6ad55d9ffb3eba4");
-define("SERVER-1", "us-cdbr-east-06.cleardb.net");
-define("USERNAME-1", "b9c65028092515");
-define("PASSWD-1", "9bbf3880");
+
+define("DATABASE", "heroku_6ad55d9ffb3eba4");
+define("SERVER", "us-cdbr-east-06.cleardb.net");
+define("USERNAME", "b9c65028092515");
+define("PASSWD", "9bbf3880");
 
 ?>
 
